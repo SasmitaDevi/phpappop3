219649cfa4d390569f80de8cef70734d6627425c
SasmitaDevi <40880688+SasmitaDevi@users.noreply.github.com>
2024-11-17 02:58:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	<h1>MySQL Table Viewer</h1>
 	<?php
 		// Define database connection variables
-		$servername = "myglsqlsvr.mysql.database.azure.com";
+		$servername = "myglsqlsvr";
 		$username = "mysqluser";
 		$password = "Supersecret!";
 		$dbname = "mysqldb";
